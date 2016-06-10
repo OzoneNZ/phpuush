b6e8a9bd0104609627713b45bbb22fbd771b6a8c
Blake Irwin <blake@proxycha.in>
2016-06-11 03:39:46 +1200

    Rework client-checking middleware
    Now has more sanitisation of incoming requests

--- app/Http/Middleware/ClientCheck.php
+++ app/Http/Middleware/ClientCheck.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
+
 use App;
 use Closure;
 use Request;
@@ -26,8 +28,24 @@ class ClientCheck
             return App::abort(403);
 
         // Check for the 'k' API key UNLESS the auth page is being hit
-        if (!Request::is('api/auth') && !$request->has('k'))
+        if (Request::is('api/auth'))
+            return $next($request);
+
+        // Check if an API key was supplied
+        if (!$request->has('k'))
             return App::abort(403);
+
+        // Check the API key is valid and the user is enabled
+        $user = User::where('api_key', $request->get('k'))
+            ->where('enabled', true)
+            ->first();
+
+        // See if a user match was found
+        if (!$user)
+            return App::abort(403);
+
+        // Share user information with all controllers
+        $request->attributes->add([ 'user' => $user ]);
 
         // Proceed with request
         return $next($request);
